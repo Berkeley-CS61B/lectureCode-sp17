77967eb10390e855adc60f5993e225bb6804358d
Aditya Kotak <adikotak@users.noreply.github.com>
2017-01-23 17:41:53 -0800

    finished extra problem
    extraIntListpractice

--- lists1/exercises/ExtraIntListPractice.java
+++ lists1/exercises/ExtraIntListPractice.java
@@ -3,16 +3,28 @@ public class ExtraIntListPractice {
       * each element incremented by x. L is not allowed
       * to change. */
     public static IntList incrList(IntList L, int x) {
-        /* Your code here. */
-        return L;        
+        if (L.rest == null)
+        {
+            return new IntList(L.first + x, null);
+        }
+        else
+        {
+            return new IntList(L.first + x, incrList(L.rest, x));
+        }
     }
 
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
-        return L;
+        IntList Q = L;
+        while (Q != null)
+        {
+            Q.first += x;
+            Q = Q.rest;
+        }
+        Q = L;
+        return Q;
     }
 
     public static void main(String[] args) {
@@ -26,8 +38,8 @@ public class ExtraIntListPractice {
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(L.get(1));
+        System.out.println(incrList(L, 3));
+        System.out.println(dincrList(L, 3));        
     }
 }
