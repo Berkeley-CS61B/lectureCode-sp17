0f6666d321228097654490349762a8c71b739fca
Josh Hug <hug@cs.berkeley.edu>
2017-01-30 10:17:20 -0800

    generic AList completed

--- lists4/live/AList.java
+++ lists4/live/AList.java
@@ -12,28 +12,39 @@
  size: The number of items in the list should be size.
 */
 
-public class AList {
-    private int[] items;
+public class AList<Item> {
+    private Item[] items;
     private int size;
 
     /** Creates an empty list. */
     public AList() {
-        items = new int[100];
+        items = (Item[]) new Object[100];
         size = 0;
     }
 
+    /** Resizes the underlying array to the target capacity. */
+    private void resize(int capacity) {
+        Item[] a = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+
     /** Inserts X into the back of the list. */
-    public void addLast(int x) {
+    public void addLast(Item x) {
+        if (size == items.length) {
+            resize(size + 1);
+        }
+
         items[size] = x;
         size = size + 1;
     }
 
     /** Returns the item from the back of the list. */
-    public int getLast() {
+    public Item getLast() {
         return items[size - 1];
     }
     /** Gets the ith item in the list (0 is the front). */
-    public int get(int i) {
+    public Item get(int i) {
         return items[i];
     }
 
@@ -44,8 +55,9 @@ public class AList {
 
     /** Deletes item from back of the list and
       * returns deleted item. */
-    public int removeLast() {
-        int x = getLast();
+    public Item removeLast() {
+        Item x = getLast();
+        items[size - 1] = null;
         size = size - 1;
         return x;
     }
