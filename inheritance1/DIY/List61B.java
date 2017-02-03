53753447019a1341ccc26d7f44dc839230553702
Josh Hug <hug@cs.berkeley.edu>
2017-02-03 14:33:00 -0800

    inheritance lecture code

---
+++ inheritance1/DIY/List61B.java
@@ -0,0 +1,6 @@
+/* TODO: Set up this interface so that it covers all common
+ * methods to AList and SLList. Also add a default method
+ * print that prints a list. */
+public interface List61B<Item> {
+
+} 
