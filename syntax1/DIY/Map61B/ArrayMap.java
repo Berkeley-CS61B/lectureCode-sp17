66a5e8184da4561ad34cfba75a6d503659f79801
Josh Hug <hug@cs.berkeley.edu>
2017-02-15 13:48:03 -0800

    added lecture 13 code

---
+++ syntax1/DIY/Map61B/ArrayMap.java
@@ -0,0 +1,50 @@
+package Map61B;
+
+import org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * An array based implementation of the Map61B class.
+ */
+public class ArrayMap<K, V> implements Map61B<K, V> {
+    public ArrayMap() {
+    }
+
+    /** Returns the index of the given key if it exists,
+     *  -1 otherwise. */
+    private int keyIndex(K key) {
+        return 0;    
+    }
+
+
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    public void put(K key, V value) {
+    }
+
+    public V get(K key) {
+        return false
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public List<K> keys() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Map61B<String, Integer> m = new ArrayMap<String, Integer>();
+        m.put("horse", 3);
+        m.put("fish", 9);
+        m.put("house", 10);
+        int expected = 9;
+        assertEquals((Integer) expected, m.get("fish"));
+    }
+}
