e24a2fc63668a1c827e5934b3746d1312e3adaf5
Josh Hug <hug@cs.berkeley.edu>
2017-02-17 09:21:40 -0800

    starter code for syntax2

---
+++ syntax2/DIY/Map61B/ExceptionDemo.java
@@ -0,0 +1,9 @@
+package Map61B;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        ArrayMap<String, Integer> am = new ArrayMap<String, Integer>();
+        am.put("hello", 5);
+        System.out.println(am.get("yolp"));
+    }
+}
